d4103b665c7996c74b11b0f216cf64640b03c12a
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-02-04 21:12:43 +0600

    anaToVori done

--- math.js
+++ math.js
@@ -1,3 +1,4 @@
 const myNumber = -5;
 const output = Math.abs(myNumber);
 console.log(output);
+console.log("adamm"+"eve");
---
+++ prooblem1.js
@@ -0,0 +1,6 @@
+function anaToVori(ana){
+    const vori= ana/12;
+    return vori;
+}
+const vories= anaToVori(48);
+console.log(vories);
