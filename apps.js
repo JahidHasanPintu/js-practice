67bc0e88be5f6af2ec16d38fe761912fba03701e
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-03-11 12:01:12 +0600

    apps updated

--- apps.js
+++ apps.js
@@ -1,30 +1,30 @@
-let posts=[ ];
+let posts = [];
 
 const likedPostsId = [];
 const reportedPostsId = [];
 
 const getLikedPosts = () => {
-    return posts.filter((post) => likedPostsId.includes(post.id));
+  return posts.filter((post) => likedPostsId.includes(post.id));
 };
 
 const getReportedPosts = () => {
-    return posts.filter((post) => reportedPostsId.includes(post.id));
+  return posts.filter((post) => reportedPostsId.includes(post.id));
 };
 
 const isLiked = (id) => {
-    return likedPostsId?.length && !!likedPostsId.includes(id);
+  return likedPostsId?.length && !!likedPostsId.includes(id);
 };
 
 const addToLiked = (id) => {
-  // Like button problem solved 
-    likedPostsId.push(id); 
-    showPosts(posts);
+  // like pblm solved 
+  likedPostsId.push(id);
+  showPosts(posts);
 };
 
 const reportPost = (id) => {
-    reportedPostsId.push(id);
-    const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
-    showPosts(remainingPosts);
+  reportedPostsId.push(id);
+  const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
+  showPosts(remainingPosts);
 };
 
 const displayContent = (text) => {
@@ -33,30 +33,30 @@ const displayContent = (text) => {
 };
 
 const switchTab = (id) => {
-    if (id === "posts") {
-        document.getElementById( "posts" ).style.display = "grid";
-        document.getElementById( "liked" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
-    } else if (id === "liked") {
-        document.getElementById( "liked" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
+  if (id === "posts") {
+    document.getElementById("posts").style.display = "grid";
+    document.getElementById("liked").style.display = "none";
+    document.getElementById("reported").style.display = "none";
+  } else if (id === "liked") {
+    document.getElementById("liked").style.display = "block";
+    document.getElementById("posts").style.display = "none";
+    document.getElementById("reported").style.display = "none";
 
-        displayLikedPosts();
-    } else {
-        document.getElementById( "reported" ).style.display = "block";
-        document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "liked" ).style.display = "none";
+    displayLikedPosts();
+  } else {
+    document.getElementById("reported").style.display = "block";
+    document.getElementById("posts").style.display = "none";
+    document.getElementById("liked").style.display = "none";
 
-        displayReportedPosts();
-    }
+    displayReportedPosts();
+  }
 };
 
 const createPost = (post) => {
-    const image = post.image;
-    const div = document.createElement( "article" );
-    div.classList.add( "post" );
-    div.innerHTML = `
+  const image = post.image;
+  const div = document.createElement("article");
+  div.classList.add("post");
+  div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
                   <a
@@ -64,7 +64,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -97,9 +97,8 @@ const createPost = (post) => {
 
                   <div class="post__indicators"></div>
 
-                  <button class="post__button post__button--align-right" onclick="reportPost(${
-                      post.id
-                  })">
+                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id
+    })">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
@@ -122,26 +121,26 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
                 </div>
               </div>
       `;
-    return div;
+  return div;
 };
 
 const showPosts = (posts) => {
-    const productsContainer = document.getElementById( "posts" );
-    productsContainer.innerHTML = "";
+  const productsContainer = document.getElementById("posts");
+  productsContainer.innerHTML = "";
 
-    posts.forEach((post) => {
-        const div = createPost(post);
-        productsContainer.appendChild(div);
-    });
+  posts.forEach((post) => {
+    const div = createPost(post);
+    productsContainer.appendChild(div);
+  });
 };
 
 const displayLikedPosts = () => {
@@ -156,7 +155,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  // report post show error fixed
+  // report post show solved
   document.getElementById("reported").innerHTML = '';
   reportedPosts.forEach((post) => {
     const div = createPost(post);
@@ -164,7 +163,7 @@ const displayReportedPosts = () => {
   });
 };
 
-const loadPosts = async () =>{
+const loadPosts = async () => {
   let data = await fetch('../data/posts.json');
   posts = await data.json();
   showPosts(posts);
